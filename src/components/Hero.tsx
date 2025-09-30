22db9023d4be848dc3ca74b383f3cbe453a1fe2e
Rad Sled <35455227+RadSled@users.noreply.github.com>
2025-09-29 19:17:35 -0600

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,43 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
+import { Box } from "@mui/material";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF } from "@react-three/drei";
-
-function Model() {
-  const gltf = useGLTF("/models/jump2.gltf");
-  return <primitive object={gltf.scene} />;
-}
+import Model from "../Model";
+import Play from "../../images/playBtn.inline.svg";
+import Pause from "../../images/pauseBtn.inline.svg";
 
 const Hero = () => {
+  const [anim, setAnim] = useState(true);
+
   return (
-    <div style={{ width: "100%", height: "600px" }}>
-      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
+    <Box component="div" sx={{ position: "relative", width: "100%", height: 600 }}>
+      <Canvas>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <directionalLight intensity={0.5} />
         <Suspense fallback={null}>
-          <Model />
+          {anim && <Model />}
         </Suspense>
-        <OrbitControls />
       </Canvas>
-    </div>
+
+      <Box
+        component="span"
+        onClick={() => setAnim((a) => !a)}
+        sx={{
+          width: 40,
+          height: 40,
+          border: "1px solid #1CE7C2",
+          borderRadius: "50%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          cursor: "pointer",
+          position: "absolute",
+          right: 20,
+          bottom: 20,
+        }}
+      >
+        {anim ? <Pause /> : <Play />}
+      </Box>
+    </Box>
   );
 };
 
