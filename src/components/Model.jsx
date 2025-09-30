99e1862e3fbea9509130b55fa74e5ded75f16f79
Rad Sled <35455227+RadSled@users.noreply.github.com>
2025-09-29 19:10:11 -0600

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,24 +1,7 @@
-import React, { useRef, useEffect } from "react";
-import { useLoader, useFrame } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { AnimationMixer } from "three";
+import React from "react";
+import { useGLTF } from "@react-three/drei";
 
-export default function Model() {
-  const gltf = useLoader(GLTFLoader, "/models/jump2.gltf");
-  const mixer = useRef();
-
-  useEffect(() => {
-    if (gltf.animations.length > 0) {
-      mixer.current = new AnimationMixer(gltf.scene);
-      gltf.animations.forEach((clip) => {
-        mixer.current.clipAction(clip).play();
-      });
-    }
-  }, [gltf]);
-
-  useFrame((state, delta) => {
-    if (mixer.current) mixer.current.update(delta);
-  });
-
+export function Model() {
+  const gltf = useGLTF("/models/jump2.gltf");
   return <primitive object={gltf.scene} />;
 }
