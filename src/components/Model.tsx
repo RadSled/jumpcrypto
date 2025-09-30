beef78fb5e15d89c6aa4634e14d1fb9aa42cee0f
Rad Sled <35455227+RadSled@users.noreply.github.com>
2025-09-29 19:45:38 -0600

    Update Model.tsx

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,28 +1,30 @@
-import React, { useEffect } from "react";
-import { useGLTF, useAnimations } from "@react-three/drei";
-import * as THREE from "three";
+import React, { useRef } from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { Group } from 'three';
+
+type GLTFResult = GLTF & {
+  nodes: any;
+  materials: any;
+  animations: any[];
+};
 
 const Model = () => {
-  // Make sure the path matches your public folder
-  const { scene, animations } = useGLTF("/static/jump2.gltf");
-  const { actions } = useAnimations(animations, scene);
+  const group = useRef<Group>(null);
+  const { scene, animations } = useGLTF('/models/jump2.gltf') as GLTFResult;
+  const { actions } = useAnimations(animations, group);
 
-  // Play all animations
-  useEffect(() => {
-    if (actions) {
-      Object.values(actions).forEach((action) => action.play());
+  // Play all animations automatically
+  React.useEffect(() => {
+    if (animations.length > 0) {
+      animations.forEach((_, i) => {
+        actions[Object.keys(actions)[i]]?.play();
+      });
     }
+  }, [animations, actions]);
 
-    // Fix grey material
-    scene.traverse((child) => {
-      if (child.isMesh) {
-        child.material.side = THREE.DoubleSide;
-        child.material.needsUpdate = true;
-      }
-    });
-  }, [actions, scene]);
-
-  return <primitive object={scene} scale={0.01} position={[0, -1, 0]} />;
+  return <primitive ref={group} object={scene} scale={1} />;
 };
 
 export default Model;
+
+useGLTF.preload('/models/jump2.gltf');
