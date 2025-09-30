437b8009205b6667a0bc82dea5a854a05e64c09f
Rad Sled <35455227+RadSled@users.noreply.github.com>
2025-09-29 19:09:45 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import Hero from './components/Hero';
+import React from "react";
+import Hero from "./components/Hero";
 
-const App = () => <Hero />;
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Hero />
+    </div>
+  );
+}
 
 export default App;
