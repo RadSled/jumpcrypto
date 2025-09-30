b7dae455ac6f2fcec91494c22effa0a8a8df943f
Rad Sled <35455227+RadSled@users.noreply.github.com>
2025-09-29 18:53:50 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import Hero from "./components/Hero";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
